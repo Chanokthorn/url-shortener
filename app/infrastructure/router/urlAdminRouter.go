9e6271e2dc1311d9e02e5432b88ba9d057d08dd5
chanokthorn <chanokthorn.uer@bkp.co.th>
2021-05-03 11:54:04 +0700

    added admin token middleware

--- app/infrastructure/router/urlAdminRouter.go
+++ app/infrastructure/router/urlAdminRouter.go
@@ -2,10 +2,12 @@ package router
 
 import (
 	"github.com/labstack/echo/v4"
+	"url-shortener/app/infrastructure/middleware"
 	"url-shortener/app/interface/controller"
 )
 
-func CreateURLAdminRouter(g *echo.Group, c controller.URLAdminController) {
+func CreateURLAdminRouter(g *echo.Group, c controller.URLAdminController, m *middleware.AdminTokenMiddleware) {
+	g.Use(m.CheckTokenMiddleware)
 	g.GET("/url", c.ListURL)
 	g.DELETE("/url/:short-code", c.DeleteURL)
 }
